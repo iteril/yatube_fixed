70369940bbd1868ba521124567317bcad975b5aa
iteril <annko0606@mail.ru>
2026-04-21 22:03:59 +0300

    Fixed structure: manage.py in root

---
+++ api/urls.py
@@ -0,0 +1,14 @@
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
+from rest_framework.authtoken import views
+from .views import PostViewSet, GroupViewSet, CommentViewSet
+
+router = DefaultRouter()
+router.register(r'posts', PostViewSet)
+router.register(r'groups', GroupViewSet)
+router.register(r'posts/(?P<post_id>\d+)/comments', CommentViewSet, basename='comment')
+
+urlpatterns = [
+    path('', include(router.urls)),
+    path('api-token-auth/', views.obtain_auth_token),
+]
